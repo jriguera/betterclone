06317e85d13e2ee2edb28c61af225125abf1572e
Jose Riguera <jriguera@gmail.com>
2020-01-01 22:18:42 +0100

    fix

--- make-deb.sh
+++ make-deb.sh
@@ -4,7 +4,7 @@ set -o pipefail  # exit if pipe command fails
 [ -z "$DEBUG" ] || set -x
 
 mkdir -p deb
-rm -rf dev/*
+rm -rf deb/*
 dpkg-buildpackage -rfakeroot -us -uc -b
 mv -f ../betterclone_* deb/
 
